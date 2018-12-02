6b8bac93af22bb084a1d3639f1724c1a0b2ecdf8
Tech-A06 <hibah28@live.co.uk>
2018-12-02 01:35:39 +0000

    added gl-matrix, mat4 is not working, 3d object not completed

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
+
 var vertexShaderText =
   [
     'precision mediump float;',
     '',
-    'attribute vec2 vertPosition;',
+    'attribute vec3 vertPosition;',
     'attribute vec3 vertColor;',
     'varying vec3 fragColor;',
+    'uniform mat4 mWorld;',
+    'uniform mat4 mView;',
+    'uniform mat4 mProj;',
     '',
     'void main()',
     '{',
     '  fragColor = vertColor;',
-    '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+    '  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
     '}'
   ].join('\n');
 
@@ -44,11 +48,11 @@ const initDemo = () => {
 
   canvas.height = 600;
   canvas.width = 800;
-  gl.viewport(0, 0, 800, 600);
 
   // rgba setting the color only 
   gl.clearColor(0.75, 0.85, 0.8, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT)
+  gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT);
+
 
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -83,25 +87,47 @@ const initDemo = () => {
   ///BUFFER
   ///
 
-  const triangleVertices = [// [x, y, R,G,B
-    0.0, 0.5, 1.0, 0.0, 0.0,
-    -0.5, -0.5, 0.0, 0., 1.0,
-    0.5, -0.5, 0.0, 1.0, 0.
+  const triangleVertices = [// [x, y, z R,G,B
+    0.0, 0.5, 0.0, 1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.0, 0.0, 0.0, 1.0,
+    0.5, -0.5, 0.0, 0.0, 1.0, 0.0
   ];
+
+
+
   const trianglevertexBufferObject = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, trianglevertexBufferObject); // binding to the active buffer
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);// the buffer takes in 64bits and the vertices is in 32
 
+
   const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
   const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-  gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, gl.FALSE, 5 * Float32Array.BYTES_PER_ELEMENT, 0);  //(attribute location, number of elements, type of elements, bool, size of individual vertex, offset from beginning of a single vertex to this attribute)
-  gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, gl.FALSE, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+  gl.vertexAttribPointer(positionAttribLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0);  //(attribute location, number of elements, type of elements, bool, size of individual vertex, offset from beginning of a single vertex to this attribute)
+  gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
 
   gl.enableVertexAttribArray(positionAttribLocation);
   gl.enableVertexAttribArray(colorAttribLocation);
 
+  gl.useProgram(program);
+
+  const matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+  const matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+  const matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+  const worldMatrix = new Float32Array(16);
+  const viewMatrix = new Float32Array(16);
+  const projMatrix = new Float32Array(16);
+
+  gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+  gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+  gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+
+  mat4.identity(worldMatrix);
+  mat4.identity(viewMatrix);
+  mat4.identity(projMatrix);
+
   /// Main render loop
 
-  gl.useProgram(program);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 };
