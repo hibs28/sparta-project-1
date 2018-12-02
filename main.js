29e5847fa99d2022ec27f32b4c0433cebca0424a
Tech-A06 <hibah28@live.co.uk>
2018-12-02 20:47:19 +0000

    it didnt work out

--- main.js
+++ main.js
@@ -1,107 +1,20 @@
-var vertexShaderText =
-  [
-    'precision mediump float;',
-    '',
-    'attribute vec2 vertPosition;',
-    'attribute vec3 vertColor;',
-    'varying vec3 fragColor;',
-    '',
-    'void main()',
-    '{',
-    '  fragColor = vertColor;',
-    '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
-    '}'
-  ].join('\n');
+// an attribute will receive data from a buffer
+attribute vec4 a_position;
 
-var fragmentShaderText =
-  [
-    'precision mediump float;',
-    '',
-    'varying vec3 fragColor;',
-    'void main()',
-    '{',
-    '  gl_FragColor = vec4(fragColor, 1.0);',
-    '}'
-  ].join('\n');
+// all shaders have a main function
+void main() {
 
+  // gl_Position is a special variable a vertex shader
+  // is responsible for setting
+  gl_Position = a_position;
+}
 
-const initDemo = () => {
-  const canvas = document.createElement('canvas');
-  document.querySelector('body').appendChild(canvas);
-  const gl = canvas.getContext('webgl');
-  if (!gl) {
-    console.log("WebGl not supported, falling back on experimental WebGL");
-    // gl = canvas.getContext('experimental-gl');
-  }
+// fragment shaders don't have a default precision so we need
+// to pick one. mediump is a good default. It means "medium precision"
+precision mediump float;
 
-  if (!gl) {
-    alert("Your browser does not support WebGL")
-  }
-  // canvas.width = window.innerWidth;
-  // canvas.height = window.innerHeight;
-  // gl.viewport(0, 0, window.innerWidth, window.innerHeight);
-
-
-  canvas.height = 600;
-  canvas.width = 800;
-  gl.viewport(0, 0, 800, 600);
-
-  // rgba setting the color only 
-  gl.clearColor(0.75, 0.85, 0.8, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT)
-
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-  gl.shaderSource(vertexShader, vertexShaderText);
-  gl.shaderSource(fragmentShader, fragmentShaderText);
-
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.error('ERROR compiling vertex shader! ', gl.getShaderInfoLog(vertexShader));
-    return;
-  }
-  gl.compileShader(fragmentShader)
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-    return;
-  }
-
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-  }
-  gl.validateProgram(program);
-  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-    console.error('ERROR validating program', gl.getProgramInfoLog(program));
-  }
-
-  ///
-  ///BUFFER
-  ///
-
-  const triangleVertices = [// [x, y, R,G,B
-    0.0, 0.5, 1.0, 0.0, 0.0,
-    -0.5, -0.5, 0.0, 0., 1.0,
-    0.5, -0.5, 0.0, 1.0, 0.
-  ];
-  const trianglevertexBufferObject = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, trianglevertexBufferObject); // binding to the active buffer
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);// the buffer takes in 64bits and the vertices is in 32
-
-  const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-  const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-  gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, gl.FALSE, 5 * Float32Array.BYTES_PER_ELEMENT, 0);  //(attribute location, number of elements, type of elements, bool, size of individual vertex, offset from beginning of a single vertex to this attribute)
-  gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, gl.FALSE, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
-
-  gl.enableVertexAttribArray(positionAttribLocation);
-  gl.enableVertexAttribArray(colorAttribLocation);
-
-  /// Main render loop
-
-  gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-};
+void main() {
+  // gl_FragColor is a special variable a fragment shader
+  // is responsible for setting
+  gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+}
